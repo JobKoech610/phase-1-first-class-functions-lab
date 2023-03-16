f00fe0ae32fa1b4112620e09ffc2251316525126
Job koech <jobkoech610@gmail.com>
2023-03-16 16:53:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function (drivers){
+return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function (drivers){
+    return drivers.slice(-2);
+    }
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier = function(integer){
+return (function(fare){
+return fare*integer;
+})
+}
+
+const fareDoubler = function(fare){
+    return createFareMultiplier(2)(fare)
+}
+
+const fareTripler = function(fare){
+    return createFareMultiplier(3)(fare)
+}
+
+function selectDifferentDrivers(drivers,func){
+return func(drivers)
+}
+
